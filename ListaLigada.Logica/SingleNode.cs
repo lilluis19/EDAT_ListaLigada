cbe6e815e7cccc2bbb5da91faab7afbba0312473
lilluis19 <23030157@itesa.edu.mx>
2024-10-30 07:26:11 -0600

    Versión_02:Metodo Agregar y ToString

---
+++ ListaLigada.Logica/SingleNode.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ListaLigada.Logica
+{
+    public class SingleNode<T>
+    {
+        public T? Data { get; set; }
+        public SingleNode<T> Next {get;  set; }
+
+        public SingleNode(T data) 
+        {
+            Data = data;
+            Next = null;
+        }
+
+    }
+}
--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using ListaLigada.Logica;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World! Lista ligada simple \n");
+
+        var singleList = new SingleList<String>();
+
+        singleList.Add("Juan");
+        singleList.Add("Jose");
+        singleList.Add("Jan");
+
+        Console.WriteLine(singleList);
     }
 }
